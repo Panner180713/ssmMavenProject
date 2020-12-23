9e22824faec2151bf25dc819ce3bfb581590e443
chenshoukai <chenshoukai@msunsoft.com>
2020-12-23 09:17:01 +0800

    使用注解启动定时器

--- src/main/java/scheduler/SchedulerTask.java
+++ src/main/java/scheduler/SchedulerTask.java
@@ -1,5 +1,6 @@
 package scheduler;
 
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component;
  * @Date 2020/03/12 9:27
  */
 @Component
+@EnableScheduling
 public class SchedulerTask {
 
     @Scheduled(fixedRate = 1000*5)
