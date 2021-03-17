22e6bd4c9f05b6fccbd207b5d6696e40f7d76c59
chenshoukai <chenshoukai@msunsoft.com>
2021-03-17 11:22:45 +0800

    提交

--- src/main/java/listener/sessionExpireListener.java
+++ src/main/java/listener/sessionExpireListener.java
@@ -1,20 +1,33 @@
 package listener;
 
+import org.apache.shiro.session.Session;
+import org.apache.shiro.session.SessionListener;
+
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
 /**
+ * 1.spring+shiro下HttpSessionListener接口监听Session失效的问题：
+ * spring使用shiro后，由于shiro重新封装了原有的session，所以不能再使用原来的session监听方法了，
+ * 要实现shiro的listener进行监听，并将其配置给sessionManager
  * @Author chenshoukai
  * @Date 2020/11/06 14:53
  */
-public class sessionExpireListener implements HttpSessionListener {
+public class sessionExpireListener implements SessionListener {
+
+
     @Override
-    public void sessionCreated(HttpSessionEvent httpSessionEvent) {
+    public void onStart(Session session) {
 
     }
 
     @Override
-    public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
-        System.out.println("httpsession过期了...");
+    public void onStop(Session session) {
+        System.out.println("sessionExpireListener onStop...");
+    }
+
+    @Override
+    public void onExpiration(Session session) {
+        System.out.println("sessionExpireListener onExpiration...");
     }
 }
